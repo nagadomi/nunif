395c8ec9f9868c119537d6f0b45355a7c3dbd4ca
nagadomi <nagadomi@gmail.com>
2022-12-16 10:01:41 +0900

    Use CosineAnnealingWarmRestarts instead of CosineAnnealingLR

--- nunif/training/trainer.py
+++ nunif/training/trainer.py
@@ -4,7 +4,7 @@ import argparse
 from multiprocessing import cpu_count
 import torch
 import torch.optim as optim
-from torch.optim.lr_scheduler import StepLR, MultiStepLR, CosineAnnealingLR
+from torch.optim.lr_scheduler import StepLR, MultiStepLR, CosineAnnealingWarmRestarts
 from ..models import create_model, save_model, load_model
 from ..initializer import set_seed
 from abc import ABC, abstractmethod
@@ -103,14 +103,14 @@ class Trainer(ABC):
                     milestones=self.args.learning_rate_decay_step,
                     gamma=self.args.learning_rate_decay)
         elif self.args.scheduler == "cosine":
-            step = self.args.learning_rate_cycles * 2 - 1
-            t_max = self.args.max_epoch // step
-            if self.args.max_epoch % step != 0:
-                old_max_epoch = self.args.max_epoch
-                self.args.max_epoch -= self.args.max_epoch % step
-                print(f"scheduler=cosine: max_epoch: {old_max_epoch} -> {self.args.max_epoch}")
+            step = self.args.learning_rate_cycles
+            t_0 = self.args.max_epoch // step
+            old_max_epoch = self.args.max_epoch
+            # Adjust epoch to keep the final epoch to the minimum LR
+            self.args.max_epoch -= (self.args.max_epoch % step) + 1
+            print(f"scheduler=cosine: max_epoch: {old_max_epoch} -> {self.args.max_epoch}")
             eta_min = self.args.learning_rate * 1e-3
-            return CosineAnnealingLR(self.optimizer, T_max=t_max, eta_min=eta_min)
+            return CosineAnnealingWarmRestarts(self.optimizer, T_0=t_0, eta_min=eta_min)
 
     def create_grad_scaler(self):
         return torch.cuda.amp.GradScaler(enabled=self.amp_is_enabled())
@@ -175,7 +175,7 @@ def create_trainer_default_parser():
     parser.add_argument("--learning-rate-decay-step", type=int, nargs="+", default=[1],
                         help="learning rate decay step for StepLR/MultiStepLR")
     parser.add_argument("--learning-rate-cycles", type=int, default=5,
-                        help="number of learning rate cycles for CosineAnnealingLR")
+                        help="number of learning rate cycles for CosineAnnealingWarmRestarts")
     parser.add_argument("--disable-amp", action="store_true",
                         help="disable AMP for some special reason")
     parser.add_argument("--resume", action="store_true",
