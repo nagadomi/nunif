dd710b42c31a20a42548d77bd020d4295fdf01f3
nagadomi <nagadomi@gmail.com>
2023-01-02 03:17:08 +0900

    Add playground/vae

--- nunif/modules/functional.py
+++ nunif/modules/functional.py
@@ -3,3 +3,24 @@ import torch
 
 def inplace_clip(x, min_value, max_value):
     return torch.clamp_(x, min_value, max_value)
+
+# for VAE
+
+def gaussian_noise(mean, log_var):
+    # NOTE:
+    # math.exp(log_var * 0.5) == math.exp(log_var/math.log(math.e**2)) == math.sqrt(math.exp(log_var))
+    standard_deviation = torch.exp(log_var * 0.5)
+    noise = torch.randn(mean.shape, device=mean.get_device())
+    return mean + (noise * standard_deviation)
+
+
+def gaussian_kl_divergence_loss(mean, log_var, reduction="mean"):
+    var = torch.exp(log_var)
+    mean2 = mean ** 2
+    kl = 0.5 * (1 + log_var - mean2 - var)
+    if reduction == "mean":
+        kl = kl.mean()
+    elif reduction == "sum":
+        # assuming shape[0] is batch dim
+        kl = kl.sum()
+    return -kl
---
+++ playground/vae/README.md
@@ -0,0 +1,15 @@
+# VAE
+
+Repository for playing/studying with Variational Autoencoder.
+
+# MNIST Map
+
+```
+python3 -m playground.vae.train_mnist --data-dir ./tmp/vae --model-dir ./tmp/vae
+```
+The generated images are saved in `tmp/vae`.
+
+![vae](figure/vae_mnist.png)
+
+See also [SOM](../som).
+
