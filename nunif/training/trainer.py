b055f94e88e8dceb6ad4beb3d987982022138935
nagadomi <nagadomi@gmail.com>
2023-01-12 09:31:09 +0900

    Change the default amp dtype to fp16
    bfloat32 prevents NaN, but degrades the accuracy of SR model.

--- nunif/training/trainer.py
+++ nunif/training/trainer.py
@@ -206,8 +206,8 @@ def create_trainer_default_parser():
                         help="learning rate for warmup")
     parser.add_argument("--disable-amp", action="store_true",
                         help="disable AMP for some special reason")
-    parser.add_argument("--amp-float", type=str, default="bfloat16", choices=["bfloat16", "float16"],
-                        help="dtype for autocast")
+    parser.add_argument("--amp-float", type=str, default="fp16", choices=["bfloat16", "fp16"],
+                        help="dtype for autocast. bfloat/fp16")
     parser.add_argument("--resume", action="store_true",
                         help="resume training from the latest checkpoint file")
     parser.add_argument("--reset-state", action="store_true",
