00c62eba9d8e5b958d498ca4d25fcf5aae06cc68
nagadomi <nagadomi@gmail.com>
2023-01-12 09:30:03 +0900

    Revert "Change default eta=0.01 in ClampLoss"
    This reverts commit 81f40eb8513ae44da2d2d7f8bbbf8cc92b164f50.

--- nunif/modules/clamp_loss.py
+++ nunif/modules/clamp_loss.py
@@ -5,7 +5,7 @@ from torch import nn
 class ClampLoss(nn.Module):
     """ Wrapper Module for `(clamp(input, 0, 1) - clamp(target, 0, 1))`
     """
-    def __init__(self, module, min_value=0, max_value=1, eta=0.01):
+    def __init__(self, module, min_value=0, max_value=1, eta=0.001):
         super().__init__()
         self.module = module
         self.min_value = min_value
