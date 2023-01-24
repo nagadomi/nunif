0ec110487d74592bab41e2c7641fe65e9437a3d1
nagadomi <nagadomi@gmail.com>
2023-01-25 07:02:01 +0900

    waifu2x: unlimited: Reset running flag when Error

--- waifu2x/web/unlimited_waifu2x/public_html/script.js
+++ waifu2x/web/unlimited_waifu2x/public_html/script.js
@@ -593,7 +593,9 @@ $(function () {
         $.cookie("tile_random", $("input[name=tile_random]").prop("checked"), {expires: g_expires});
     });
     window.addEventListener("unhandledrejection", function(e) {
-        set_message("Error: " + e.toString(), -1);
-        console.error(e);
+        set_message("(-_-) Error: " + e.reason, -1);
+        // reset running flags
+        onnx_runner.running = false;
+        onnx_runner.stop_flag = false;
     });
 });
