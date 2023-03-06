3f097a36963b5a9bb2c0028da1b16111e7974783
nagadomi <nagadomi@gmail.com>
2023-03-06 13:04:54 +0900

    Remake alex11

--- nunif/modules/alex11_loss.py
+++ nunif/modules/alex11_loss.py
@@ -7,16 +7,12 @@ from os import path
 
 class Alex11Loss(nn.Module):
     # AlexNet's first 11x11 linear filter
-    def __init__(self, in_channels, loss=None, cosine_weight=0.0):
+    def __init__(self, in_channels):
         assert in_channels in {1, 3}
         super().__init__()
         self.in_channels = in_channels
-        self.cosine_weight = cosine_weight
         self.conv = self.create_filter(in_channels)
-        if loss is None:
-            self.loss = CharbonnierLoss(reduction="none")
-        else:
-            self.loss = loss
+        self.loss = CharbonnierLoss(reduction="none")
 
     @staticmethod
     def create_filter(in_channels):
@@ -32,16 +28,17 @@ class Alex11Loss(nn.Module):
             f = net.features[0].weight.data.detach().clone()
             # rgb to grayscale
             f = f[:, 0:1, :, :] * 0.29891 + f[:, 1:2, :, :] * 0.58661 + f[:, 2:3, :, :] * 0.11448
-            # normalize [-1, 1]
-            for i in range(f.shape[0]):
-                f[i] *= 1. / torch.abs(f[i]).max()
             # index 31 is most similar to the identity filter,
             # so override it with the true identity filter
             f[31, :, :, :].zero_()
-            f[31, :, 11 // 2, 11 // 2] = 1.  # 1., 4., 8.
+            f[31, :, 11 // 2, 11 // 2] = 1.
+            # normalize l2_norm=1
+            for i in range(f.shape[0]):
+                f[i] *= 1. / torch.sqrt((f[i] ** 2).sum())
             torch.save(f, weight_path)
         # conv2d
-        conv = nn.Conv2d(in_channels, 64 * in_channels, kernel_size=11, stride=1, padding=0, groups=in_channels, bias=False)
+        conv = nn.Conv2d(in_channels, 64 * in_channels, kernel_size=11, stride=1, padding=0,
+                         groups=in_channels, bias=False)
         f = torch.cat([f for _ in range(in_channels)], dim=0)
         conv.weight.data.copy_(f)
         for m in conv.parameters():
@@ -49,14 +46,10 @@ class Alex11Loss(nn.Module):
         return conv
 
     def forward(self, input, target):
-        y = self.conv(input)
-        t = self.conv(target)
-        loss_filter = self.loss(y, t).mean()
-        if self.cosine_weight > 0:
-            loss_cosine = -F.cosine_similarity(y, t).mean()
-            loss = loss_filter * (1. - self.cosine_weight) + loss_cosine * self.cosine_weight
-        else:
-            loss = loss_filter
+        y = self.conv(input * 2. - 1.)
+        t = self.conv(target * 2. - 1.)
+        loss = self.loss(y, t)
+        loss = torch.amax(loss, dim=1, keepdim=True).mean()
 
         return loss
 
