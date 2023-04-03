34c267069bca7ea5068e90ff5f8d3ae1f4dedcb2
nagadomi <nagadomi@gmail.com>
2023-04-03 21:56:38 +0900

    Add SNSEBlock

--- nunif/modules/attention.py
+++ nunif/modules/attention.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+from torch.nn.utils.parametrizations import spectral_norm
 
 
 class SEBlock(nn.Module):
@@ -11,6 +12,22 @@ class SEBlock(nn.Module):
         super(SEBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, in_channels // reduction, 1, 1, 0, bias=bias)
         self.conv2 = nn.Conv2d(in_channels // reduction, in_channels, 1, 1, 0, bias=bias)
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        z = F.adaptive_avg_pool2d(x, 1)
+        z = self.conv1(z)
+        z = F.relu(z, inplace=True)
+        z = self.conv2(z)
+        z = torch.sigmoid(z)
+        return x * z.expand(x.shape)
+
+
+class SNSEBlock(nn.Module):
+    def __init__(self, in_channels, reduction=8, bias=True):
+        super().__init__()
+        self.conv1 = spectral_norm(nn.Conv2d(in_channels, in_channels // reduction, 1, 1, 0, bias=bias))
+        self.conv2 = spectral_norm(nn.Conv2d(in_channels // reduction, in_channels, 1, 1, 0, bias=bias))
 
     def forward(self, x):
         b, c, _, _ = x.size()
