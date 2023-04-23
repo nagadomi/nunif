f16bc506b3332bb389168502c2f20d8d665ebe9e
Nergis <127741113+Nergis-Dev@users.noreply.github.com>
2023-04-23 23:02:13 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 echo "Install dependencies packages"
-echo "Install List: [git-core, libmagickwand-dev, libsnappy-dev, libraqm-dev, python-is-python3, python3, python3-pip, python3-dev]"
+echo "Install List: [git-core, libmagickwand-dev, libsnappy-dev, libraqm-dev, python-is-python3, python3, python3-pip, python3-dev, python3-tk]"
 echo "Do you want to continue?"
 echo -n "Yes or No: "
 read agreement
@@ -7,13 +7,13 @@ if [ $agreement = "yes" ]
 then
     sudo apt update -y
     sudo apt upgrade -y
-    sudo apt install git-core libmagickwand-dev libsnappy-dev libraqm-dev python-is-python3 python3 python3-pip python3-dev -y
+    sudo apt install git-core libmagickwand-dev libsnappy-dev libraqm-dev python-is-python3 python3 python3-pip python3-dev python3-tk -y
     echo "Done!"
 elif [ $agreement = "y" ]
 then
     sudo apt update -y
     sudo apt upgrade -y
-    sudo apt install git-core libmagickwand-dev libsnappy-dev libraqm-dev python-is-python3 python3 python3-pip python3-dev -y
+    sudo apt install git-core libmagickwand-dev libsnappy-dev libraqm-dev python-is-python3 python3 python3-pip python3-dev python3-tk -y
     echo "Done!"
 else
     echo "Exit!"
