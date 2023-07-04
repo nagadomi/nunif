73b2057ea49486a1294707c19d44604ba417de1e
My12123 <49092250+My12123@users.noreply.github.com>
2023-07-04 13:58:18 +0300

    Installer for Windows CPU.bat

---
+++ Installer for Windows CPU.bat
@@ -0,0 +1,8 @@
+# Install python 3.10
+python -m venv venv
+call .\venv\Scripts\activate
+pip3 install torch torchvision torchaudio torchtext
+pip install -r requirements.txt
+python -m waifu2x.download_models
+python -m waifu2x.web.webgen
+pause
