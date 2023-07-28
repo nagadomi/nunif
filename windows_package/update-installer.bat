89867df7f53de1a700efa9d7a6efb178ab1636c5
nagadomi <nagadomi@gmail.com>
2023-07-29 00:55:38 +0900

    windows_package: Update setenv.bat for git

--- windows_package/update-installer.bat
+++ windows_package/update-installer.bat
@@ -4,6 +4,7 @@ setlocal enabledelayedexpansion
 call %~dp0\setenv.bat
 
 copy %NUNIF_DIR%\windows_package\update.bat %~dp0\update.bat
+copy %NUNIF_DIR%\windows_package\setenv.bat %~dp0\setenv.bat
 
 pause
 exit /b 0
