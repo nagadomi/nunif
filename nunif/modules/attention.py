f10d4e47a1b0febf1e552985110a0fbe46051748
nagadomi <nagadomi@gmail.com>
2023-12-30 02:05:03 +0900

    Add MHA, WindowMHA2d

--- nunif/modules/attention.py
+++ nunif/modules/attention.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.parametrizations import spectral_norm
+from .permute import bchw_to_bnc, bnc_to_bchw
 
 
 class SEBlock(nn.Module):
@@ -50,6 +51,55 @@ class SNSEBlock(nn.Module):
         z = self.conv2(z)
         z = torch.sigmoid(z)
         return x * z.expand(x.shape)
+
+
+class MHA(nn.Module):
+    def __init__(self, embed_dim, num_heads):
+        super().__init__()
+        # require torch >= 2.0 (recommend torch >= 2.1.2)
+        # nn.MultiheadAttention also has a bug with float attn_mask, so PyTorch 2.1 is required anyway.
+        assert hasattr(F, "scaled_dot_product_attention"), "torch version does not support F.scaled_dot_product_attention"
+
+        assert embed_dim % num_heads == 0
+        self.num_heads = num_heads
+        self.qkv_proj = nn.Linear(embed_dim, embed_dim * 3)
+        self.head_proj = nn.Linear(embed_dim, embed_dim)
+
+    def forward(self, x, attn_mask=None, dropout_p=0.0, is_causal=False):
+        B, N, C = x.shape  # batch, sequence, feature
+        q, k, v = self.qkv_proj(x).split(C, dim=-1)
+        # B, H, N, C // H
+        q = q.view(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        k = k.view(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        v = v.view(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        x = F.scaled_dot_product_attention(q, k, v,
+                                           attn_mask=attn_mask, dropout_p=dropout_p,
+                                           is_causal=is_causal)
+        # B, N, (H, C // H)
+        x = x.permute(0, 2, 1, 3).reshape(B, N, C)
+        x = self.head_proj(x)
+        return x
+
+
+class WindowMHA2d(nn.Module):
+    """ WindowMHA
+    BCHW input/output
+    """
+    def __init__(self, in_channels, num_heads, window_size=(4, 4)):
+        super().__init__()
+        self.window_size = (window_size if isinstance(window_size, (tuple, list))
+                            else (window_size, window_size))
+        self.num_heads = num_heads
+        self.mha = MHA(in_channels, num_heads)
+
+    def forward(self, x, attn_mask=None):
+        src = x
+        out_shape = src.shape
+        x = bchw_to_bnc(x, self.window_size)
+        x = self.mha(x, attn_mask=attn_mask)
+        x = bnc_to_bchw(x, out_shape, self.window_size)
+
+        return x
 
 
 def _test():
