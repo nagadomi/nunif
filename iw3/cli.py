0074490997a0bda7d603029a432faec69d0a7c90
nagadomi <nagadomi@gmail.com>
2024-02-24 19:05:01 +0900

    iw3: Show GPU Max Memory Allocated when DEBUG=1

--- iw3/cli.py
+++ iw3/cli.py
@@ -1,5 +1,8 @@
+import torch
 from .utils import create_parser, set_state_args, iw3_main
 from . import models # noqa
+from nunif.logger import logger
+from nunif.device import device_is_cuda
 
 
 def main():
@@ -7,6 +10,10 @@ def main():
     args = parser.parse_args()
     set_state_args(args)
     iw3_main(args)
+
+    if device_is_cuda(args.state["device"]):
+        max_vram_mb = int(torch.cuda.max_memory_allocated(args.state["device"]) / (1024 * 1024))
+        logger.debug(f"GPU Max Memory Allocated {max_vram_mb}MB")
 
 
 if __name__ == "__main__":
