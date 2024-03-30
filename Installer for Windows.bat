2d371cfe7ca5f69c300bb5848cde5fcae5efd9bd
nagadomi <nagadomi@gmail.com>
2024-03-30 19:09:47 +0900

    Update Installer for Windows.bat

--- Installer for Windows.bat
+++ Installer for Windows.bat
@@ -1,9 +1,10 @@
 @rem Install python 3.10
 python -m venv venv
 call .\venv\Scripts\activate
-pip install -r requirements-torch.txt
-pip install -r requirements.txt
-pip install -r requirements-gui.txt
+pip install --upgrade --no-cache-dir -r requirements-torch.txt
+pip install --upgrade --no-cache-dir -r requirements.txt
+pip install --upgrade --no-cache-dir -r requirements-gui.txt
 python -m waifu2x.download_models
 python -m waifu2x.web.webgen
+python -m iw3.download_models
 pause
