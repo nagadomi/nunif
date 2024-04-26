ac5d9a42cc02d3476d4b3dd09bb556629a8e5abb
nagadomi <nagadomi@gmail.com>
2024-04-26 16:01:24 +0900

    iw3: Support chained mapper

--- iw3/mapper.py
+++ iw3/mapper.py
@@ -25,7 +25,22 @@ def distance_to_disparity(x, c):
     return ((c / (c1 - x)) - min_v) / (1.0 - min_v)
 
 
-def get_mapper(name):
+def inv_distance_to_disparity(x, c):
+    return ((c + 1) * x) / (x + c)
+
+
+def shift(x, value):
+    return (1.0 - value) * x + value
+
+
+def div_shift(x, value, c=0.6):
+    x = inv_distance_to_disparity(x, c)
+    x = (1.0 - value) * x + value
+    x = distance_to_disparity(x, c)
+    return x
+
+
+def resolve_mapper_function(name):
     # https://github.com/nagadomi/nunif/assets/287255/0071a65a-62ff-4928-850c-0ad22bceba41
     if name == "pow2":
         return lambda x: x ** 2
@@ -68,30 +83,60 @@ def get_mapper(name):
             "div_1": 0.1,
         }[name]
         return lambda x: distance_to_disparity(x, param)
+    elif name in {"shift_25", "shift_50", "shift_75"}:
+        param = {
+            "shift_25": 0.25,
+            "shift_50": 0.5,
+            "shift_75": 0.75,
+        }[name]
+        return lambda x: shift(x, param)
+    elif name in {"div_shift_25", "div_shift_50", "div_shift_75"}:
+        param = {
+            "div_shift_25": 0.25,
+            "div_shift_50": 0.5,
+            "div_shift_75": 0.75,
+        }[name]
+        return lambda x: div_shift(x, param, 0.6)
     else:
         raise NotImplementedError(f"mapper={name}")
 
 
+def chain(x, functions):
+    for f in functions:
+        x = f(x)
+    return x
+
+
+def get_mapper(name):
+    if ":" in name:
+        names = name.split(":")
+    else:
+        names = [name]
+    functions = [resolve_mapper_function(name) for name in names]
+    return lambda x: chain(x, functions)
+
+
 def resolve_mapper_name(mapper, foreground_scale, model_name):
+    disparity_mapper = None
     if mapper is not None:
         if mapper == "auto":
             if model_name == "DepthAnything":
-                mapper = "none"
+                disparity_mapper = "none"
             elif model_name == "ZoeDepth":
-                mapper = "div_6"
+                disparity_mapper = "div_6"
             else:
                 raise ValueError(f"Unsupported model_name {model_name}")
         else:
-            pass
+            disparity_mapper = mapper
     else:
         if model_name == "DepthAnything":
-            mapper = [
+            disparity_mapper = [
                 "inv_mul_3", "inv_mul_2", "inv_mul_1",
                 "none",
                 "mul_1", "mul_2", "mul_3",
             ][foreground_scale + 3]
         elif model_name == "ZoeDepth":
-            mapper = [
+            disparity_mapper = [
                 "none", "div_25", "div_10",
                 "div_6",
                 "div_4", "div_2", "div_1",
@@ -99,4 +144,6 @@ def resolve_mapper_name(mapper, foreground_scale, model_name):
         else:
             raise ValueError(f"Unsupported model_name {model_name}")
 
-    return mapper
+    assert disparity_mapper is not None
+
+    return disparity_mapper
