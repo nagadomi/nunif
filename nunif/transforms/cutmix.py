7acc277a817fb7b999889e08d1bce508e6744055
nagadomi <nagadomi@gmail.com>
2024-06-24 20:56:29 +0900

    Supports cutmix from a single image

--- nunif/transforms/cutmix.py
+++ nunif/transforms/cutmix.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageOps
 import random
 from torchvision import transforms as T
 from torchvision.transforms import (
@@ -40,14 +40,26 @@ def generate_random_mask(width, height, mask_min=0.2, mask_max=0.5, rotate_p=0.2
     return mask
 
 
-def cutmix(a, b, mask_min=0.2, mask_max=0.5, rotate_p=0.2):
+def cutmix(a, b=None, mask_min=0.2, mask_max=0.5, rotate_p=0.2):
     # a, b: PIL image
+    if b is None:
+        # a and b are the same image
+        # make grid
+        hflip = ImageOps.mirror(a)
+        b = Image.new(a.mode, (a.width * 2, a.height * 2), "black")
+        b.paste(a, (0, 0))
+        b.paste(hflip, (a.width, 0))
+        b.paste(hflip, (0, a.height))
+        b.paste(a, (a.width, a.height))
+        a, b = b, a
 
     # crop to the same size
     width = min(a.width, b.height)
     height = min(a.height, b.height)
-    a = _random_crop(a, width, height)
-    b = _random_crop(b, width, height)
+    if a.width != width or a.height != height:
+        a = _random_crop(a, width, height)
+    if b.width != width or b.height != height:
+        b = _random_crop(b, width, height)
     # composite
     mask = generate_random_mask(width, height, mask_min=mask_min, mask_max=mask_max, rotate_p=rotate_p)
     out = Image.composite(a, b, mask)
@@ -59,7 +71,14 @@ if __name__ == "__main__":
     a = Image.open("cc0/bottle.jpg")
     b = Image.open("cc0/lighthouse.jpg")
 
-    for i in range(10):
-        out = cutmix(a, b)
-        out.show()
-        time.sleep(1)
+    if False:
+        for i in range(10):
+            out = cutmix(a, b)
+            out.show()
+            time.sleep(1)
+
+    if True:
+        for i in range(10):
+            out = cutmix(b)
+            out.show()
+            time.sleep(1)
