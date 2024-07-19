8c997c379b880b918f7e0d11dcade7274991decc
nagadomi <nagadomi@gmail.com>
2024-07-19 17:53:42 +0900

    Add softmax based AdaptiveWeight
    it is constant values at the time of inference

--- nunif/modules/fusion.py
+++ nunif/modules/fusion.py
@@ -13,17 +13,37 @@ class Lerp(nn.Module):
         else:
             self.weight = nn.Parameter(torch.zeros(weight_shape, dtype=torch.float32))
 
-
     def forward(self, input, end):
         # out = input + (0. 5 + self.weight) * (end - start)
         return torch.lerp(input, end, (0.5 + self.weight).to(input.dtype))
 
 
-class PLerp2d(nn.Module):
-    def __init__(self, in_channels):
+class AdaptiveWeight(nn.Module):
+    def __init__(self, n=2):
+        assert n >= 2
         super().__init__()
-        self.proj = nn.Conv2d(in_channels * 2, in_channels, kernel_size=1, bias=True)
+        self.weight = nn.Parameter(torch.ones((n,), dtype=torch.float32))
 
-    def forward(self, x1, x2):
-        w = F.sigmoid(self.proj(torch.cat((x1, x2), dim=1)))
-        return torch.lerp(x1, x2, w)
+    def forward(self):
+        # exp(x_i) / sum([exp(x_j) for x_j in x])
+        return F.softmax(self.weight, dim=0)
+
+
+class AdaptiveWeightedAdd(nn.Module):
+    def __init__(self, in_channels, n=2):
+        super().__init__()
+        self.adaptive_weight = AdaptiveWeight(n=n)
+
+    def forward(self, *inputs):
+        weight = self.adaptive_weight()
+        # print(inputs[0].shape[1], weight)
+
+        assert weight.numel() == len(inputs)
+        return sum([x * weight[i] for i, x in enumerate(inputs)])
+
+
+if __name__ == "__main__":
+    avg = AdaptiveWeightedAdd(32, 4)
+    x = torch.ones((4, 32, 4, 4))
+    x = avg(x, x, x, x)
+    assert x.sum() == x.numel()
