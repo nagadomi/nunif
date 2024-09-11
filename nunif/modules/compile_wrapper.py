09b8b0da2db332598239615fcea7c532711be43e
nagadomi <nagadomi@gmail.com>
2024-09-11 17:51:29 +0900

    Add environment variable to allow disabling compile
    NUNIF_DISABLE_COMPILE=1 python -m ...

--- nunif/modules/compile_wrapper.py
+++ nunif/modules/compile_wrapper.py
@@ -1,8 +1,9 @@
 import torch
 import sys
+import os
 
 
-_DISABLE_COMPILE = sys.platform != "linux"
+_DISABLE_COMPILE = sys.platform != "linux" or os.getenv("NUNIF_DISABLE_COMPILE", False)
 
 
 def compile(*args, **kwargs):
