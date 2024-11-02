8e441d6fd6e2dbe5e2d13a32d2700871afd046fb
nagadomi <nagadomi@gmail.com>
2024-11-02 22:55:26 +0900

    iw3: Download depth_pro repo with iw3.download_models

--- iw3/download_models.py
+++ iw3/download_models.py
@@ -1,10 +1,12 @@
 from . import zoedepth_model as ZU
 from . import depth_anything_model as DU
+from . import depth_pro_model as PU
 
 
 def main():
     ZU.force_update()
     DU.force_update()
+    PU.force_update()
     if not ZU.has_model():
         ZU.load_model(gpu=-1)
 
