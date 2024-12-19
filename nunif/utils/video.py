f208fae3b4b6e661f6b697c901a59e461a270065
nagadomi <nagadomi@gmail.com>
2024-12-20 00:27:56 +0900

    Retry os.replace() in process_video()

--- nunif/utils/video.py
+++ nunif/utils/video.py
@@ -10,6 +10,7 @@ import re
 import torch
 from concurrent.futures import ThreadPoolExecutor
 from fractions import Fraction
+import time
 
 
 # Add video mimetypes that does not exist in mimetypes
@@ -600,6 +601,19 @@ def configure_video_codec(config):
             config.pix_fmt = "gbrp"
 
 
+def try_replace(output_path_tmp, output_path):
+    try_count = 4
+    while try_count >= 0:
+        try:
+            os.replace(output_path_tmp, output_path)
+            break
+        except: # noqa
+            time.sleep(2)
+            try_count -= 1
+            if try_count <= 0:
+                raise
+
+
 def process_video(input_path, output_path,
                   frame_callback,
                   config_callback=default_config_callback,
@@ -751,7 +765,7 @@ def process_video(input_path, output_path,
     if not (stop_event is not None and stop_event.is_set()):
         # success
         if path.exists(output_path_tmp):
-            os.replace(output_path_tmp, output_path)
+            try_replace(output_path_tmp, output_path)
 
 
 def generate_video(output_path,
@@ -866,7 +880,7 @@ def generate_video(output_path,
     if not (stop_event is not None and stop_event.is_set()):
         # success
         if path.exists(output_path_tmp):
-            os.replace(output_path_tmp, output_path)
+            try_replace(output_path_tmp, output_path)
 
 
 def process_video_keyframes(input_path, frame_callback, min_interval_sec=4., title=None, stop_event=None, suspend_event=None):
