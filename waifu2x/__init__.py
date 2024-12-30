eb3ecea89759c73927d945c18a2bf52c7aa20f68
nagadomi <nagadomi@gmail.com>
2024-12-30 17:12:06 +0900

    waifu2x: Set WAIFU2X_WEB env variable in __init__.py
    sys.orig_argv is only available in 3.10 or later so it is not set in 3.9.
    No serious problem without setting it, as it only controls compiling for now.

--- waifu2x/__init__.py
+++ waifu2x/__init__.py
@@ -1,5 +1,10 @@
 import os
+import sys
 os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
+
+if "waifu2x.web" in getattr(sys, "orig_argv", []):
+    os.environ["WAIFU2X_WEB"] = "1"
+
 try:
     import truststore
     truststore.inject_into_ssl()
