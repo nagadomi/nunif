cb9374df830da025f82f8be880f94d1be66154bf
nagadomi <nagadomi@gmail.com>
2024-12-31 22:49:29 +0900

    waifu2x: Update winc_unet
    as for 4x, maybe the final version

--- waifu2x/models/winc_unet.py
+++ waifu2x/models/winc_unet.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from nunif.models import I2IBaseModel, register_model, register_model_factory
 from nunif.modules.attention import WindowMHA2d, WindowScoreBias
-from nunif.modules.replication_pad2d import ReplicationPad2dNaive as ReplicationPad2dNaive
+from nunif.modules.replication_pad2d import ReplicationPad2dNaive as ReplicationPad2dNaive, replication_pad2d_naive
 from nunif.modules.init import icnr_init, basic_module_init
 from nunif.modules.compile_wrapper import conditional_compile
 
@@ -92,7 +92,8 @@ class WACBlock(nn.Module):
 
 
 class WACBlocks(nn.Module):
-    def __init__(self, in_channels, num_heads=4, qkv_dim=None, window_size=8, mlp_ratio=2, num_layers=2, padding=True, conv_mlp=True):
+    def __init__(self, in_channels, num_heads=4, qkv_dim=None,
+                 window_size=8, mlp_ratio=2, num_layers=2, padding=True, conv_mlp=True):
         super(WACBlocks, self).__init__()
         if isinstance(window_size, int):
             window_size = [window_size] * num_layers
@@ -111,33 +112,52 @@ class WACBlocks(nn.Module):
 
 
 class Overscan(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        C = in_channels
+        padding = 1 + 2 + 3 + 1
+        self.pad = ReplicationPad2dNaive((padding,) * 4, detach=True)
+        self.conv1 = nn.Conv2d(C, C, kernel_size=3, dilation=1, stride=1, padding=0)
+        self.conv2 = nn.Conv2d(C, C // 2, kernel_size=3, dilation=2, stride=1, padding=0)
+        self.conv3 = nn.Conv2d(C // 2, C // 2, kernel_size=3, dilation=3, stride=1, padding=0)
+        self.fuse = nn.Sequential(
+            nn.Conv2d(C + C // 2 + C // 2, C, kernel_size=3, stride=1, padding=0),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(C, C, kernel_size=1, stride=1, padding=0))
+        basic_module_init(self)
+
+    def forward(self, x):
+        x = self.pad(x)
+        x1 = self.conv1(x)
+        x1 = F.leaky_relu(x1, 0.2, inplace=True)
+        x2 = self.conv2(x1)
+        x2 = F.leaky_relu(x2, 0.2, inplace=True)
+        x3 = self.conv3(x2)
+        x3 = F.leaky_relu(x3, 0.2, inplace=True)
+        x1 = F.pad(x1, (-(2 + 3),) * 4)
+        x2 = F.pad(x2, (-3,) * 4)
+        x4 = torch.cat([x1, x2, x3], dim=1)
+        x = self.fuse(x4)
+        return x
+
+
+class IR(nn.Module):
     def __init__(self, in_channels=3, out_channels=16):
         super().__init__()
-        C = out_channels * 2 * 2
-        self.proj = nn.Conv2d(in_channels * 2 * 2, C, kernel_size=1, stride=1, padding=0)
-        self.mha1 = WindowMHA2d(C, num_heads=2, window_size=8)
-        self.mha2 = WindowMHA2d(C, num_heads=2, window_size=6)
-        self.relative_bias1 = WindowScoreBias(window_size=8)
-        self.relative_bias2 = WindowScoreBias(window_size=6)
-        self.mlp = nn.Sequential(
-            nn.Conv2d(C, C, kernel_size=1, stride=1, padding=0),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(C, C, kernel_size=3, stride=1, padding=0),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
-        basic_module_init(self.proj)
-        basic_module_init(self.mlp)
+        self.patch = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=0)
+        self.overscan = Overscan(out_channels)
+        self.fusion = nn.Conv2d(out_channels * 2, out_channels, kernel_size=3, stride=1, padding=0)
+        basic_module_init(self)
 
     @conditional_compile(["NUNIF_TRAIN", "WAIFU2X_WEB"])
     def forward(self, x):
-        x = F.pixel_unshuffle(x, 2)
-        assert x.shape[2] % 8 == 0
-        x = self.proj(x)
-        x = x + self.mha1(x, attn_mask=self.relative_bias1())
-        x = F.pad(x, (-1,) * 4) + self.mlp(x)
-        assert x.shape[2] % 6 == 0
-        x = x + self.mha2(x, attn_mask=self.relative_bias2())
-        x = F.pixel_shuffle(x, 2)
+        x = replication_pad2d_naive(x, (1,) * 4)
+        x = self.patch(x)
+        x = F.leaky_relu(x, 0.2, inplace=True)
+        ov = self.overscan(x)
+        x = torch.cat([x, ov], dim=1)
+        x = replication_pad2d_naive(x, (1,) * 4)
+        x = self.fusion(x)
         return x
 
 
@@ -213,7 +233,8 @@ class SourceResidual(nn.Module):
     @conditional_compile(["NUNIF_TRAIN", "WAIFU2X_WEB"])
     def forward(self, x, src):
         # print(self.scale_bias)
-        # torch.save(self.proj_source.state_dict(), "tmp/nn_upsample/weight.pth")
+        # torch.save(self.resampling.state_dict(), "tmp/nn_upsample/weight.pth")
+        src = replication_pad2d_naive(src, (1,) * 4)
         src = self.resampling(src)
         if self.scale_factor > 1:
             src = F.pixel_shuffle(src, self.scale_factor)
@@ -239,10 +260,9 @@ class WincUNetBase(nn.Module):
         HEADS = max(C // 32, 2)
         HEADS2 = max(C2 // 32, 2)
 
-        self.overscan = Overscan(in_channels)
         # shallow feature extractor
-        self.patch = nn.Conv2d(in_channels, C // 2, kernel_size=3, stride=1, padding=0)
-        self.fusion = nn.Conv2d(C // 2 + 16, C, kernel_size=3, stride=1, padding=0)
+        self.ir = IR(3, 16)
+        self.patch = nn.Conv2d(16, C, kernel_size=3, stride=1, padding=0)
 
         # encoder
         self.wac1 = WACBlocks(C, mlp_ratio=lv1_mlp_ratio,
@@ -262,24 +282,16 @@ class WincUNetBase(nn.Module):
 
         basic_module_init(self.patch)
         basic_module_init(self.wac1_proj)
-        basic_module_init(self.fusion)
 
         self.tile_mode = False
 
     def set_tile_mode(self):
         self.tile_mode = True
 
-    def _forward(self, x):
-        src = x
-        ov = self.overscan(x)
+    def _forward(self, x, src):
         x = self.patch(x)
+        x = F.pad(x, (-7,) * 4)
         x = F.leaky_relu(x, 0.2, inplace=True)
-        x = F.pad(x, (-1,) * 4)
-        x = torch.cat([x, ov], dim=1)
-        x = self.fusion(x)
-        x = F.pad(x, (-5,) * 4)
-        x = F.leaky_relu(x, 0.2, inplace=True)
-
         x1 = self.wac1(x)
         x = self.down1(x1)
         x = self.wac2(x)
@@ -292,37 +304,89 @@ class WincUNetBase(nn.Module):
 
         return z
 
-    def _forward_tile2x2(self, x):
+    def _forward_tile2x2(self, x, src):
         tl = x[:, :, :64, :64]
         tr = x[:, :, :64, -64:]
         bl = x[:, :, -64:, :64]
         br = x[:, :, -64:, -64:]
         x = torch.cat([tl, tr, bl, br], dim=0).contiguous()
-        x = self._forward(x)
+
+        tl = src[:, :, :64, :64]
+        tr = src[:, :, :64, -64:]
+        bl = src[:, :, -64:, :64]
+        br = src[:, :, -64:, -64:]
+        src = torch.cat([tl, tr, bl, br], dim=0).contiguous()
+
+        x = self._forward(x, src)
         tl, tr, bl, br = x.split(x.shape[0] // 4, dim=0)
         top = torch.cat([tl, tr], dim=3)
         bottom = torch.cat([bl, br], dim=3)
         x = torch.cat([top, bottom], dim=2).contiguous()
         return x
 
-    def forward(self, x):
+    def forward(self, x, src=None):
+        if src is None:
+            src = x
+            x = self.ir(x)
+        else:
+            assert x.shape[1] == 16
+
         if self.tile_mode:
             B, C, H, W = x.shape
             if self.scale_factor == 4:
-                assert H == 110 and W == H
-                assert H == 110 and W == H
-                pass
+                assert H == 112 and W == H
+                if self.training:
+                    # randomly use 64x64 2x2 tile and 112x112 1x1 tile
+                    if torch.rand((1,)).item() > 0.5:
+                        # 112 -> 110
+                        x = F.pad(x, (-1,) * 4)
+                        src = F.pad(src, (-1,) * 4)
+                        return self._forward_tile2x2(x, src)
+                    else:
+                        return self._forward(x, src)
+                else:
+                    x = F.pad(x, (-1,) * 4)
+                    src = F.pad(src, (-1,) * 4)
+                    return self._forward_tile2x2(x, src)
             else:
                 raise NotImplementedError()
-            return self._forward_tile2x2(x)
         else:
-            return self._forward(x)
+            return self._forward(x, src)
 
 
 def tile_size_validator(size):
     return (size > 16 and
             (size - 16) % 12 == 0 and
             (size - 16) % 16 == 0)
+
+
+class IRMixIn():
+    def has_callback(self):
+        # Not used
+        # The basic idea of this is,
+        # ```
+        # intermediate_representation = ir(overall_image)
+        # result_image = tiled_infer(intermediate_representation)
+        # ```
+        # Instead of tiling the image pixels directly,
+        # tiling from intermediate feature that use larger receptive field
+        return False
+
+    def config_callback(self, x):
+        C, H, W = x.shape
+        return 16 + 3, H, W, C
+
+    def preprocess_callback(self, rgb, padding):
+        batch = True
+        if rgb.ndim == 3:
+            rgb = rgb.unsqueeze(0)
+            batch = False
+        rgb = replication_pad2d_naive(rgb, padding)
+        x = self.unet.ir(rgb)
+        x = torch.cat([rgb, x], dim=1)
+        if not batch:
+            x = x.squeeze(0)
+        return x.contiguous()
 
 
 @register_model
@@ -388,7 +452,12 @@ class WincUNet4x(I2IBaseModel):
                                  scale_factor=4, last_layers=3)
 
     def forward(self, x):
-        z = self.unet(x)
+        if x.shape[1] == 16 + 3:
+            src, x = x.split([3, 16], dim=1)
+            z = self.unet(x, src)
+        else:
+            z = self.unet(x)
+
         if self.training:
             return z
         else:
@@ -491,15 +560,18 @@ def _bench(name, compile):
         print(model.name, model.i2i_offset, model.i2i_scale, f"{param:,}", f"compile={compile}")
 
     # benchmark
+    torch.cuda.synchronize()
     t = time.time()
     with torch.inference_mode(), torch.autocast(device_type="cuda"):
         for _ in range(100):
             z = model(x)
+    torch.cuda.synchronize()
     print(time.time() - t)
 
 
 if __name__ == "__main__":
     enable_full_compile = False
+
     _bench("waifu2x.winc_unet_1x", enable_full_compile)
     _bench("waifu2x.winc_unet_2x", enable_full_compile)
     _bench("waifu2x.winc_unet_4x", enable_full_compile)
