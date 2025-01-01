ce9396cd96c2e2643081aa039a33e73034a992e1
nagadomi <nagadomi@gmail.com>
2025-01-01 23:08:49 +0900

    Make truststore.inject_into_ssl() run only once

--- iw3/__init__.py
+++ iw3/__init__.py
@@ -1,7 +1,9 @@
 import os
-os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = '1'
+os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = "1"
 try:
     import truststore
-    truststore.inject_into_ssl()
+    if not os.environ.get("NUNIF_TRUSTSTORE_INJECTED", False):
+        truststore.inject_into_ssl()
+        os.environ["NUNIF_TRUSTSTORE_INJECTED"] = "1"
 except ModuleNotFoundError:
     pass
