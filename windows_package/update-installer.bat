58d60f0f9d5d970a842731645db1f16e433dd224
nagadomi <nagadomi@gmail.com>
2025-03-23 02:36:09 +0900

    windows_package: Update for iw3-desktop-gui.bat (not tested)

--- windows_package/update-installer.bat
+++ windows_package/update-installer.bat
@@ -9,6 +9,7 @@ copy /y "%NUNIF_DIR%\windows_package\install.bat" "%~dp0\install.bat"
 
 copy /y "%NUNIF_DIR%\windows_package\nunif-prompt.bat" "%~dp0\nunif-prompt.bat"
 copy /y "%NUNIF_DIR%\windows_package\iw3-gui.bat" "%~dp0\iw3-gui.bat"
+copy /y "%NUNIF_DIR%\windows_package\iw3-desktop-gui.bat" "%~dp0\iw3-desktop-gui.bat"
 copy /y "%NUNIF_DIR%\windows_package\waifu2x-gui.bat" "%~dp0\waifu2x-gui.bat"
 copy /y "%NUNIF_DIR%\windows_package\waifu2x-web.bat" "%~dp0\waifu2x-web.bat"
 
