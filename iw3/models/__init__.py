465713e4f9cc92486594d95fa22402c467475fbb
nagadomi <nagadomi@gmail.com>
2025-05-30 19:19:41 +0900

    iw3: Add DepthAA training

--- iw3/models/__init__.py
+++ iw3/models/__init__.py
@@ -1,5 +1,6 @@
 from .row_flow_v2 import RowFlowV2
 from .row_flow_v3 import RowFlowV3
 from .light_inpaint_v1 import LightInpaintV1
+from .depth_aa import DepthAA
 
 __all__ = ["RowFlowV2", "RowFlowV3", "LightInpaintV1"]
--- iw3/nunif_addon.py
+++ iw3/nunif_addon.py
@@ -3,7 +3,7 @@ from . import models # noqa
 
 
 def addon_config():
-    return [SBS3DAddon(), InpaintAddon()]
+    return [SBS3DAddon(), InpaintAddon(), DepthAAAddon()]
 
 
 class SBS3DAddon(Addon):
@@ -25,4 +25,13 @@ class InpaintAddon(Addon):
 
     def register_train(self, subparsers, default_parser):
         from .training.inpaint.trainer import register
+        return register(subparsers, default_parser)
+
+
+class DepthAAAddon(Addon):
+    def __init__(self):
+        super().__init__("iw3.depth_aa")
+
+    def register_train(self, subparsers, default_parser):
+        from .training.depth_aa.trainer import register
         return register(subparsers, default_parser)
