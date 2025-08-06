6b826104dfa98cdfbe2f6bd706c1bd0ba3fd28bf
nagadomi <nagadomi@gmail.com>
2025-08-06 15:50:41 +0900

    Update dinov2

--- nunif/modules/dinov2.py
+++ nunif/modules/dinov2.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import random
 from .weighted_loss import WeightedLoss
 from .compile_wrapper import conditional_compile
+from .pad import get_crop_size, get_pad_size
+from .reflection_pad2d import reflection_pad2d_naive
 
 
 try:
@@ -26,51 +27,41 @@ def dinov2_normalize(x):
 
 
 def dinov2_pad(x):
-    B, C, H, W = x.shape
-    pad_h = DINO_PATCH_SIZE - H % DINO_PATCH_SIZE if H % DINO_PATCH_SIZE != 0 else 0
-    pad_w = DINO_PATCH_SIZE - W % DINO_PATCH_SIZE if W % DINO_PATCH_SIZE != 0 else 0
-    if pad_h != 0 or pad_w != 0:
-        x = F.pad(x, (0, pad_w, 0, pad_h), mode="reflect")
+    pad = get_pad_size(x, DINO_PATCH_SIZE)
+    x = F.pad(x, pad, mode="reflect")
     return x
 
 
 def dinov2_crop(x):
-    B, C, H, W = x.shape
-    pad_h = H % DINO_PATCH_SIZE
-    pad_w = W % DINO_PATCH_SIZE
-    pad_h1 = pad_h // 2
-    pad_h2 = pad_h - pad_h1
-    pad_w1 = pad_w // 2
-    pad_w2 = pad_w - pad_w1
-    x = F.pad(x, (-pad_w1, -pad_w2, -pad_h1, -pad_h2))
+    pad = get_crop_size(x, DINO_PATCH_SIZE)
+    x = F.pad(x, pad)
     return x
 
 
 def dinov2_crop_pair(x, y, training=False):
     assert x.shape == y.shape
-    B, C, H, W = x.shape
-    pad_h = H % DINO_PATCH_SIZE
-    pad_w = W % DINO_PATCH_SIZE
-    if training:
-        pad_h += DINO_PATCH_SIZE
-        pad_w += DINO_PATCH_SIZE
-        pad_h1 = random.randint(0, pad_h)
-        pad_h2 = pad_h - pad_h1
-        pad_w1 = random.randint(0, pad_w)
-        pad_w2 = pad_w - pad_w1
-    else:
-        pad_h1 = pad_h // 2
-        pad_h2 = pad_h - pad_h1
-        pad_w1 = pad_w // 2
-        pad_w2 = pad_w - pad_w1
-    x = F.pad(x, (-pad_w1, -pad_w2, -pad_h1, -pad_h2))
-    y = F.pad(y, (-pad_w1, -pad_w2, -pad_h1, -pad_h2))
+    pad = get_crop_size(x, DINO_PATCH_SIZE, random_shift=training)
+    x = F.pad(x, pad)
+    y = F.pad(y, pad)
     return x, y
 
 
+def dinov2_pad_pair(x, y, training=False):
+    assert x.shape == y.shape
+    pad = get_crop_size(x, DINO_PATCH_SIZE, random_shift=training)
+    x = reflection_pad2d_naive(x, pad, detach=True)
+    y = reflection_pad2d_naive(y, pad, detach=True)
+    return x, y
+
+
+def dinov2_dim(model_type):
+    return {"vits": 384, "vitb": 768, "vitl": 1024}[model_type]
+
+
 class DINOv2IntermediateFeatures(nn.Module):
-    def __init__(self, model_type="vits", index=None):
+    def __init__(self, model_type="vits", index=None, random_projection=None):
         super().__init__()
+        self.model_type = model_type
         if index is None:
             self.intermediate_layer_index = {
                 "vits": [2, 5, 8, 11],
@@ -82,6 +73,22 @@ class DINOv2IntermediateFeatures(nn.Module):
         self.model = torch.hub.load('facebookresearch/dinov2', f"dinov2_{model_type}14_reg",
                                     verbose=False, trust_repo=True).eval()
         self.model.requires_grad_(False)
+
+        if random_projection is not None:
+            self.init_random_projection(random_projection)
+        else:
+            self.random_projection = None
+
+    def init_random_projection(self, dim):
+        feat_dim = dinov2_dim(self.model_type)
+        rng_state = torch.random.get_rng_state()
+        try:
+            torch.manual_seed(0)
+            adj = 1.0 / 14.0
+            proj = torch.randn((dim, feat_dim, 1, 1)) * (feat_dim ** -0.5) * adj
+            self.register_buffer("random_projection", proj)
+        finally:
+            torch.random.set_rng_state(rng_state)
 
     def train(self, mode=True):
         self.model.train(False)
@@ -98,18 +105,24 @@ class DINOv2IntermediateFeatures(nn.Module):
             reshape=True,
             return_class_token=False,
         )
+        if self.random_projection is not None:
+            features = [
+                F.conv2d(feat, weight=self.random_projection, bias=None, stride=1)
+                for feat in features
+            ]
+
         return features
 
 
 class DINOv2Loss(nn.Module):
-    def __init__(self, loss, model_type="vits", index=None, normalize=True):
+    def __init__(self, loss, model_type="vits", index=None, normalize=True, random_projection=None):
         super().__init__()
         self.loss = loss
         self.normalize = normalize
-        self.dino = DINOv2IntermediateFeatures(model_type=model_type, index=None)
+        self.dino = DINOv2IntermediateFeatures(model_type=model_type, index=None, random_projection=random_projection)
 
     def forward(self, input, target):
-        input, target = dinov2_crop_pair(input, target, self.training)
+        input, target = dinov2_pad_pair(input, target, self.training)
         if self.normalize:
             input = dinov2_normalize(input)
             target = dinov2_normalize(target)
@@ -123,21 +136,56 @@ class DINOv2Loss(nn.Module):
         return loss / len(input_features)
 
 
-def cosine_loss(input, target):
-    return (1.0 - F.cosine_similarity(input, target, dim=1)).mean()
-
 class CosineLoss(nn.Module):
     def forward(self, input, target):
-        return cosine_loss(input, target)
+        return (1.0 - F.cosine_similarity(input, target, dim=1)).mean()
+
+
+class Pool(nn.Module):
+    def __init__(self, base_loss, kernel_size=3):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.base_loss = base_loss
+
+    def pool(self, x):
+        x = x + F.avg_pool2d(
+            x, kernel_size=self.kernel_size, stride=1,
+            padding=(self.kernel_size - 1) // 2,
+            count_include_pad=False
+        )
+        return x
+
+    def forward(self, input, target):
+        input = self.pool(input)
+        target = self.pool(target)
+        return self.base_loss(input, target)
 
 
 def DINOv2CosineLoss(model_type="vits", index=None, normalize=True):
-    return DINOv2Loss(CosineLoss(), model_type=model_type, index=index, normalize=normalize)
+    return DINOv2Loss(CosineLoss(), model_type=model_type, index=index, normalize=normalize, random_projection=None)
+
+
+def DINOv2PoolLoss(model_type="vits", index=None, normalize=True):
+    return DINOv2Loss(
+        Pool(nn.L1Loss()),
+        model_type=model_type, index=index,
+        normalize=normalize,
+        random_projection=64
+    )
 
 
 def DINOv2CosineWith(base_loss, weight=1.0, model_type="vits", index=None, normalize=True):
-    return WeightedLoss((base_loss, DINOv2Loss(CosineLoss(), model_type=model_type, index=index, normalize=normalize)),
-                        weights=(1.0, weight))
+    return WeightedLoss((
+        base_loss,
+        DINOv2CosineLoss(model_type=model_type, index=index, normalize=normalize)
+    ), weights=(1.0, weight))
+
+
+def DINOv2PoolWith(base_loss, weight=1.0, model_type="vits", index=None, normalize=True):
+    return WeightedLoss((
+        base_loss,
+        DINOv2PoolLoss(model_type=model_type, index=index, normalize=normalize)
+    ), weights=(1.0, weight))
 
 
 def _test_feat():
@@ -159,5 +207,37 @@ def _test_feat():
     print(loss2(x, x))
 
 
+def _test_grad():
+    import torchvision.io as io
+    from .lbp_loss import YRGBLBP
+
+    y = io.read_image("cc0/320/dog.png") / 255.0
+    y = y.unsqueeze(0)
+    x = y + (torch.rand_like(y) * 0.5)
+    x.requires_grad_(True)
+
+    x.grad = None
+    l1_loss = nn.L1Loss()
+    l1_loss(x, y).backward()
+    l1_norm, l1_max = x.grad.norm(), x.grad.abs().max()
+
+    x.grad = None
+    lbp_loss = YRGBLBP()
+    lbp_loss(x, y).backward()
+    lbp_norm, lbp_max = x.grad.norm(), x.grad.abs().max()
+
+    x.grad = None
+    dinov2_loss = DINOv2PoolLoss()  # DINOv2CosineLoss()
+    (dinov2_loss(x, y.detach())).backward()
+    dinov2_norm, dinov2_max = x.grad.norm(), x.grad.abs().max()
+
+    print("dinov2 loss norm", dinov2_norm, "max", dinov2_max)
+    print("l1 loss norm", l1_norm, "max", l1_max, "weight", l1_norm / dinov2_norm)
+    print("lbp loss norm", lbp_norm, "max", lbp_max, "weight", lbp_norm / dinov2_norm)
+    # DINOv2L2PoolLoss, weight=0.078
+    # DINOv2CosineLoss, weight=0.056
+
+
 if __name__ == "__main__":
     _test_feat()
+    _test_grad()
