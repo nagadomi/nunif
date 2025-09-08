34208ab456d51d1c216fdf718c7484e78304328f
nagadomi <nagadomi@gmail.com>
2025-09-08 14:21:28 +0900

    Refactor MultiscaleLoss

--- nunif/modules/multiscale_loss.py
+++ nunif/modules/multiscale_loss.py
@@ -7,7 +7,7 @@ from .softpool import soft_pool2d
 class MultiscaleLoss(nn.Module):
     """ Wrapper Module for `(loss(x, y) * w1 + loss(downscale(x), downscale(y)) * w2 ..`
     """
-    def __init__(self, module, scale_factors=(1, 2), weights=(0.8, 0.2), mode="bicubic", antialias=False):
+    def __init__(self, module, scale_factors=(1, 2), weights=(0.8, 0.2), mode="bilinear", antialias=None, align_corners=None):
         super().__init__()
         assert len(scale_factors) == len(weights)
         assert mode in {"bicubic", "bilinear", "avg", "softpool"}
@@ -16,10 +16,10 @@ class MultiscaleLoss(nn.Module):
         self.weights = weights
         self.mode = mode
         self.antialias = antialias
+        self.align_corners = align_corners
 
     def forward(self, input, target):
         loss = 0
-        antialias = self.mode in {"bicubic", "bilinear"} and self.antialias
         for scale_factor, weight in zip(self.scale_factors, self.weights):
             if scale_factor == 1:
                 x = input
@@ -33,9 +33,9 @@ class MultiscaleLoss(nn.Module):
                     t = soft_pool2d(target, scale_factor)
                 else:
                     x = F.interpolate(input, scale_factor=1.0 / scale_factor,
-                                      mode=self.mode, antialias=antialias)
+                                      mode=self.mode, antialias=self.antialias, align_corners=self.align_corners)
                     t = F.interpolate(target, scale_factor=1.0 / scale_factor,
-                                      mode=self.mode, antialias=antialias)
+                                      mode=self.mode, antialias=self.antialias, align_corners=self.align_corners)
             loss = loss + self.module(x, t) * weight
         return loss
 
