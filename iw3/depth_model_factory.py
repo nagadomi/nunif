0432604bf3fad583e12ca5113dea0a685857330f
nagadomi <nagadomi@gmail.com>
2025-11-16 07:29:48 +0900

    iw3: Add Any_V3_Mono model
    The conversion formula for disparity might be changed later.

--- iw3/depth_model_factory.py
+++ iw3/depth_model_factory.py
@@ -1,9 +1,10 @@
-from . zoedepth_model import ZoeDepthModel
-from . depth_anything_model import DepthAnythingModel
-from . depth_pro_model import DepthProModel
-from . video_depth_anything_model import VideoDepthAnythingModel
-from . video_depth_anything_streaming_model import VideoDepthAnythingStreamingModel
-from . null_depth_model import NullDepthModel
+from .zoedepth_model import ZoeDepthModel
+from .depth_anything_model import DepthAnythingModel
+from .depth_pro_model import DepthProModel
+from .video_depth_anything_model import VideoDepthAnythingModel
+from .video_depth_anything_streaming_model import VideoDepthAnythingStreamingModel
+from .depth_anything_v3_model import DepthAnythingV3MonoModel
+from .null_depth_model import NullDepthModel
 
 
 def create_depth_model(model_type):
@@ -12,6 +13,9 @@ def create_depth_model(model_type):
         return model
     elif DepthAnythingModel.supported(model_type):
         model = DepthAnythingModel(model_type)
+        return model
+    elif DepthAnythingV3MonoModel.supported(model_type):
+        model = DepthAnythingV3MonoModel(model_type)
         return model
     elif DepthProModel.supported(model_type):
         model = DepthProModel(model_type)
