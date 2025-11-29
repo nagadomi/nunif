d6b41256b0bca3a678ff071d525ae5f4e32e0129
nagadomi <nagadomi@gmail.com>
2025-11-29 20:16:00 +0900

    stlizer: Add support for NUNIF_HOME environment variable; Fix SuperPoint model path
    For cache_dir, it may not need to be changed, but NUNIF_HOME takes priority.

--- nunif/utils/home_dir.py
+++ nunif/utils/home_dir.py
@@ -6,11 +6,15 @@ def get_home_dir(project_name, default_path):
     if nunif_home:
         nunif_home = os.path.expanduser(nunif_home)
         return os.path.join(nunif_home, project_name)
-    else:
-        return default_path
+
+    return default_path
 
 
 def ensure_home_dir(project_name, default_path):
     home_dir = get_home_dir(project_name, default_path)
     os.makedirs(home_dir, exist_ok=True)
     return home_dir
+
+
+def is_nunif_home_set():
+    return bool(os.getenv("NUNIF_HOME"))
