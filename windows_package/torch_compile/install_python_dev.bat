59b1d3525b7a2de511496b8055e3844304b0f2ca
nagadomi <nagadomi@gmail.com>
2025-12-16 12:28:52 +0900

    windows_package: Update torch_compile/install_python_dev.bat for Python 3.12.10

--- windows_package/torch_compile/install_python_dev.bat
+++ windows_package/torch_compile/install_python_dev.bat
@@ -13,20 +13,35 @@ if %ERRORLEVEL% neq 0 (
 
 call :install_python_dev
 if !ERRORLEVEL! neq 0 goto :on_error
-echo Successfully installed python310 developer header/lib
+echo Successfully installed python developer files
 pause
 exit /b 0
 
 
 :install_python_dev
   set TMP_DIR=%ROOT_DIR%\tmp
-  set PYTHON_DEV_URL=https://github.com/nagadomi/nunif/releases/download/0.0.0/python310-dev.zip
+  set PYTHON_312_10_DEV_URL=https://github.com/nagadomi/nunif/releases/download/python_dev_release/python-3.12.10-dev.zip
+  set PYTHON_310_11_DEV_URL=https://github.com/nagadomi/nunif/releases/download/python_dev_release/python-3.10.11-dev.zip
+
+  for /f %%A in ('python -c "import sys; print('.'.join([str(v) for v in sys.version_info[0:3]]))"') do set PYVER=%%A
+  if "%PYVER%" == "3.12.10" (
+    set PYTHON_DEV_URL=%PYTHON_312_10_DEV_URL%
+  ) else if "%PYVER%" == "3.10.11" (
+    set PYTHON_DEV_URL=%PYTHON_310_11_DEV_URL%
+  ) else (
+    echo Unsupported python version: %PYVER%
+    goto on_error
+  )
+  echo Installing %PYTHON_DEV_URL%
+
+  set ZIP_FILE=%TMP_DIR%\python-%PYVER%-dev.zip
+  if not exist "%TMP_DIR%" mkdir "%TMP_DIR%"
 
   powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-             "Start-BitsTransfer -Source $env:PYTHON_DEV_URL -Destination $env:TMP_DIR\python310-dev.zip"
+             "Start-BitsTransfer -Source $env:PYTHON_DEV_URL -Destination $env:ZIP_FILE -ErrorAction Stop"
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
   powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-             "Expand-Archive -Force $env:TMP_DIR\python310-dev.zip $env:PYTHON_DIR"
+             "Expand-Archive -Force $env:ZIP_FILE $env:PYTHON_DIR -ErrorAction Stop"
   if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
   exit /b 0
