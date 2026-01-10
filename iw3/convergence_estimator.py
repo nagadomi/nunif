5f8b37074f325624ddc87709c419e497c16a44cb
nagadomi <nagadomi@gmail.com>
2026-01-10 11:02:38 +0900

    iw3: Update SOD model

--- iw3/convergence_estimator.py
+++ iw3/convergence_estimator.py
@@ -5,7 +5,7 @@ from nunif.device import create_device, autocast
 from .hub_dir import HUB_MODEL_DIR
 
 
-SOD_URL = "https://github.com/nagadomi/nunif/releases/download/0.0.0/iw3_dsod_v1_20260106.pth"
+SOD_URL = "https://github.com/nagadomi/nunif/releases/download/0.0.0/iw3_sod_v1_20260109.pth"
 
 
 class ConvergenceEstimator():
