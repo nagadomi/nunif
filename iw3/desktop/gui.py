nagadomi/nunif

commit 510f7eef017e9589b695f639149e0ca9f614f3f0

iw3: desktop: Change the default screenshot backend in the GUI.
win32: wc_cuda or wc_mp
darwin: mss

iw3/desktop/gui.py
@@ -447,10 +447,16 @@ class MainFrame(wx.Frame):
                                           choices=screenshot_backends,
                                           name="cbo_screenshot")
         self.cbo_screenshot.SetEditable(False)
-        if sys.platform == "win32" and HAS_WINDOWS_CAPTURE:
-            self.cbo_screenshot.SetSelection(2)
-        else:
-            self.cbo_screenshot.SetSelection(0)
+        default_screenshot_index = 0
+        if sys.platform == "win32":
+            if "wc_cuda" in screenshot_backends:
+                default_screenshot_index = screenshot_backends("wc_cuda")
+            elif "wc_mp" in screenshot_backends:
+                default_screenshot_index = screenshot_backends("wc_mp")
+        elif sys.platform == "darwin":
+            if "mss" in screenshot_backends:
+                default_screenshot_index = screenshot_backends.index("mss")
+        self.cbo_screenshot.SetSelection(default_screenshot_index)
 
         self.lbl_monitor_index = wx.StaticText(self.grp_processor, label=T("Monitor Index"))
         self.cbo_monitor_index = wx.ComboBox(self.grp_processor,

commit 6fcfea72f42c528a22799d2c1b5d3eeccec31a8a

iw3: desktop: Fix syntax error introduced in 510f7ee

iw3/desktop/gui.py
@@ -450,9 +450,9 @@ class MainFrame(wx.Frame):
         default_screenshot_index = 0
         if sys.platform == "win32":
             if "wc_cuda" in screenshot_backends:
-                default_screenshot_index = screenshot_backends("wc_cuda")
+                default_screenshot_index = screenshot_backends.index("wc_cuda")
             elif "wc_mp" in screenshot_backends:
-                default_screenshot_index = screenshot_backends("wc_mp")
+                default_screenshot_index = screenshot_backends.index("wc_mp")
         elif sys.platform == "darwin":
             if "mss" in screenshot_backends:
                 default_screenshot_index = screenshot_backends.index("mss")